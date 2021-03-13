353080343204df15a2051f589c857d4547e7343b
chriscauley <chris@lablackey.com>
2021-03-13 13:36:57 +0000

    adding axios

---
+++ client/src/common/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+function handleError(error) {
+  throw error;
+}
+
+const api = axios.create({ baseURL: '/api' });
+api.interceptors.response.use((r) => r.data, handleError);
+
+export default api;
