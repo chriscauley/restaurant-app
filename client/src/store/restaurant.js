ef2b2898c9bfff02e0c314e485a3d940342b5afc
chriscauley <chris@lablackey.com>
2021-03-14 13:50:52 +0000

    restaurant list

--- client/src/store/restaurant.js
+++ client/src/store/restaurant.js
@@ -1,5 +1,6 @@
 import { reactive } from 'vue'
 import api from '@/common/api'
+import slugify from '@/common/slugify'
 
 const state = reactive({
   by_id: {},
@@ -8,7 +9,11 @@ const state = reactive({
 const fetch = (page = 1) => {
   api.get('restaurant/?page=' + page).then(response => {
     response.items.forEach(restaurant => {
-      state.by_id[restaurant.id] = restaurant
+      state.by_id[restaurant.id] = {
+        img_style: `background-image: url(${restaurant.photo_url})`,
+        to: `/restaurant/${restaurant.id}/${slugify(restaurant.name)}/`,
+        ...restaurant,
+      }
     })
   })
 }
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,11 +1,27 @@
 <template>
-  <div>Home</div>
+  <div class="home">
+    <h1>Restaurants</h1>
+    <div class="restaurant-list row">
+      <div v-for="restaurant in restaurants" :key="restaurant.id" class="col-4">
+        <router-link class="card" :to="restaurant.to">
+          <div class="card__img" :style="restaurant.img_style" />
+          <div>{{ restaurant.name }}</div>
+        </router-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   __route: {
     path: '/',
+  },
+  computed: {
+    restaurants() {
+      console.log(this.$store.restaurant.all())
+      return this.$store.restaurant.all()
+    },
   },
 }
 </script>
